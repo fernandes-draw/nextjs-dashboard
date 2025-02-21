a097f0bcd79afb713ebf513e06b319f13ed3e3bc
fernandes-draw <fernandes_draw@hotmail.com>
2025-02-21 19:50:53 -0300

    Capitulo_10 enable PPR

--- app/dashboard/(overview)/page.tsx
+++ app/dashboard/(overview)/page.tsx
@@ -3,9 +3,9 @@ import LatestInvoices from '@/app/ui/dashboard/latest-invoices';
 import RevenueChart from '@/app/ui/dashboard/revenue-chart';
 import { lusitana } from '@/app/ui/fonts';
 import {
+  CardSkeleton,
   LatestInvoicesSkeleton,
   RevenueChartSkeleton,
-  CardSkeleton,
 } from '@/app/ui/skeletons';
 import { Suspense } from 'react';
 
